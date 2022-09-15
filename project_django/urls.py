73b451c9afb7fb68552da1d1b3665641032beace
vwjaya32 <vinsen.wijaya@ui.ac.id>
2022-09-15 13:23:21 +0700

    Tutorial 2 - JSON dan XML

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('wishlist/', include('wishlist.urls')),
+    path('xml/', include('wishlist.urls')),
+    path('json/', include('wishlist.urls')),
 ]

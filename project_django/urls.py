dd46b85b123615ccc432bb1e7324448bbe84c572
vwjaya32 <vinsen.wijaya@ui.ac.id>
2022-09-15 13:28:44 +0700

    Updated urls.py

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,6 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('wishlist/', include('wishlist.urls')),
-    path('xml/', include('wishlist.urls')),
-    path('json/', include('wishlist.urls')),
 ]
